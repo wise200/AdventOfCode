06c1fd8e4a45b87de85b3699433f285361b012b2
Justin Wise <jrwise200@gmail.com>
2018-12-17 00:24:31 -0600

    Created file for Day 8

---
+++ Day8.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Day8 {
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(new File("Day8.dat"));
+
+		part1(scan);
+		//part2(scan);
+	}
+
+	public static void part1(Scanner scan) {
+
+	}
+
+	public static void part2(Scanner scan) {
+
+	}
+}
